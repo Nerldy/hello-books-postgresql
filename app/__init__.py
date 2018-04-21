54ce91dd18ef885125e343dabcb015d6d9707f0d
Nerldy_ <arnodhis@gmail.com>
2018-04-21 10:56:15 +0300

    update configuration
    Add Blueprints for Users and Books

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,22 @@
 from flask import Flask
 
+"""
+---------------------
+---- CONFIG ----
+---------------------
+"""
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
+app.url_map.strict_slashes = False
 
-from . import views
+"""
+---------------------
+---- BLUEPRINTS ----
+---------------------
+"""
+from app.books.views import books_blueprint
+from app.users.views import users_blueprint
+
+# Register Blueprints
+app.register_blueprint(books_blueprint)
+app.register_blueprint(users_blueprint)
